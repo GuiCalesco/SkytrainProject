75d5c6191a5ec348dd1eb9189ddce86d812ce2ad
lucas <macuser@10.1.102.22>
2015-03-18 19:19:07 -0700

    first screen

--- ProjectSkyTrain/src/com/example/projectskytrain/MainActivity.java
+++ ProjectSkyTrain/src/com/example/projectskytrain/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.projectskytrain;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -10,26 +14,21 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main_screen);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+		setContentView(R.layout.activity_main);
+		
+		TimerTask task = new TimerTask() {
 			
-		}
-		return super.onOptionsItemSelected(item);
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				finish();
+				startActivity(new Intent(MainActivity.this,MainScreen.class));
+			}
+		};
+		
+		Timer open = new Timer();
+		open.schedule(task, 500);
 	}
+
+	
 }
---
+++ ProjectSkyTrain/src/com/example/projectskytrain/MainScreen.java
@@ -0,0 +1,15 @@
+package com.example.projectskytrain;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainScreen extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main_screen);
+	}
+
+}
