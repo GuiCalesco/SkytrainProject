2b9c4123a7da460d29cef37ca700489b0707c2d1
weslleykelson <weslleykelson1@gmail.com>
2015-03-23 17:19:56 -0700

    button

--- ProjectSkyTrain/src/com/projectskytrain/MainScreen.java
+++ ProjectSkyTrain/src/com/projectskytrain/MainScreen.java
@@ -8,10 +8,13 @@ import android.app.Activity;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.MultiAutoCompleteTextView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainScreen extends Activity {
 	String[] stnNames = new String[56];
@@ -30,10 +33,22 @@ public class MainScreen extends Activity {
                 android.R.layout.simple_dropdown_item_1line,stnNames);
 		 final AutoCompleteTextView fromStation = (AutoCompleteTextView)findViewById(R.id.txtFrom);
 		 final AutoCompleteTextView toStation = (AutoCompleteTextView)findViewById(R.id.txtTo);
-		 //final TextView test = (TextView)findViewById(R.id.textView1);
+		// final TextView test = (TextView)findViewById(R.id.textView1);
+		 Button search = (Button)findViewById(R.id.btnSearch);
+		 
+		 search.setOnClickListener(new OnClickListener() {
+		
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				//test.setText(toStation.getEditableText().toString());
+			}
+		}) ;
 		 
 		 fromStation.setAdapter(adapter);
          toStation.setAdapter(adapter);
+         
+         
         
 
 	}
