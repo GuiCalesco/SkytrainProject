cd3cd89196a2bb9e03d850573e96bf8ff6ebfe0d
jardel-lima <jardelribeiro.lima@gmail.com>
2015-03-23 17:43:13 -0700

    getTimeAndPath function was implemented

--- ProjectSkyTrain/src/com/projectskytrain/auxiliry/Station.java
+++ ProjectSkyTrain/src/com/projectskytrain/auxiliry/Station.java
@@ -1,10 +1,21 @@
 package com.projectskytrain.auxiliry;
 
+import java.util.LinkedList;
+
 import com.projectskytrain.constants.StationEnum;
 
-public abstract class Station {
-
-	private static int getStationId(String stationName){
+public class Station {
+	
+	int [][] grafoStations;
+	LinkedList<Integer> path;
+	final int numberOfStation = 56;
+	
+	public Station(){
+		grafoStations = ReadData.getData();
+		
+	}
+	
+	public static int getStationId(String stationName){
 		int id = -1;
 		
 		for (StationEnum var : StationEnum.values()) {
@@ -17,7 +28,7 @@ public abstract class Station {
 		return id;
 	}
 	
-	private static StationEnum getStationInfo( int id){
+	public static StationEnum getStationInfo( int id){
 		StationEnum station = null;
 		for (StationEnum var : StationEnum.values()) {
 			if(var.getCode()==id){
@@ -28,6 +39,12 @@ public abstract class Station {
 		
 		return station;
 	}
+	
+	public int getTimeAndPath(int stnCode1, int stnCode2){
+		return GrafoStation.shortestPath(grafoStations, numberOfStation, stnCode1, stnCode2, path);
+	}
+	
+	
 	
 
 }
