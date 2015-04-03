6346183c962fdef4a37e0529cded30b97f0daf90
weslleykelson <weslleykelson1@gmail.com>
2015-04-03 16:40:24 -0700

    Showing Map

--- ProjectSkyTrain/src/com/projectskytrain/SkytrainLine.java
+++ ProjectSkyTrain/src/com/projectskytrain/SkytrainLine.java
@@ -4,11 +4,19 @@ import com.example.projectskytrain.R;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class SkytrainLine extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.skytrain_map);
+		
+		final WebView line = (WebView)findViewById(R.id.webView1);
+		
+		String data = "<body>" + "<img src=\"skytrain_line.png\"/></body>";
+
+		line.loadDataWithBaseURL("file:///android_res/drawable/",data , "text/html", "utf-8",null);
+		
 	}
 
 }
